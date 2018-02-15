9ffadb2532a772906911d26a8f8ee74b45538d82
Juan Gabriel Lievano <gabe@jglievano.com>
2018-02-15 08:16:05 -0800

    Start project.

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Read</h1>
+        <ul>
+          <li>
+            <h2>The Meditations</h2>
+            <img src="https://images-na.ssl-images-amazon.com/images/I/415jqPZ74ZL._SX319_BO1,204,203,200_.jpg"/>
+          </li>
+        </ul>
+        <h1>To read</h1>
+        <ul>
+          <li>
+            <h2>Letters from a Stoic</h2>
+            <img src="https://images-na.ssl-images-amazon.com/images/I/51mt31ITPvL._SY346_.jpg"/>
+          </li>
+        </ul>
       </div>
     );
   }
